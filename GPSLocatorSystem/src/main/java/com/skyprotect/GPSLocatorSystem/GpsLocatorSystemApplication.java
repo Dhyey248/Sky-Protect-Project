c268e0686e3b05756fce4eb486c3de24a2169f18
Dhyey <dhyey.ghetia@sky.uk>
2025-01-27 12:49:34 +0000

    Setup: Springboot Project

---
+++ GPSLocatorSystem/src/main/java/com/skyprotect/GPSLocatorSystem/GpsLocatorSystemApplication.java
@@ -0,0 +1,13 @@
+package com.skyprotect.GPSLocatorSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsLocatorSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsLocatorSystemApplication.class, args);
+	}
+
+}
